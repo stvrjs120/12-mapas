ed135eed6d6ffb6293170bea389f400c49f1be05
Steven Rojas <stvrjs.k12@gmail.com>
2019-01-02 09:50:24 -0600

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,30 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { MaterialModule } from './material.module';
+
 import { MapaComponent } from './components/mapa/mapa.component';
+import { MapaEditarComponent } from './components/mapa/mapa-editar.component';
+
+import { AgmCoreModule } from '@agm/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
+  entryComponents: [
+    MapaEditarComponent
+  ],
   declarations: [
     AppComponent,
-    MapaComponent
+    MapaComponent,
+    MapaEditarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDptwDo60ynHQVldXVK9WULcZru_emQ-KI'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
